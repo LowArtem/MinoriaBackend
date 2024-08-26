f451c903f8012429637ea5c3885ce841d04bcb50
Artem Zhikharev <artemziharew@gmail.com>
2024-08-26 23:48:35 +0300

    fix: fix enum displaying

--- MinoriaBackend.Api/Configurations/Swagger/EnumTypesSchemaFilter.cs
+++ MinoriaBackend.Api/Configurations/Swagger/EnumTypesSchemaFilter.cs
@@ -33,9 +33,16 @@ public class EnumTypesSchemaFilter : ISchemaFilter
 
             var fullTypeName = context.Type.FullName;
 
-            foreach (var enumMemberName in schema.Enum.OfType<OpenApiString>().Select(v => v.Value))
+            // Создаём новый список для преобразованных значений enum
+            var lowerEnumValues = new List<IOpenApiAny>();
+
+            foreach (var enumMember in schema.Enum.OfType<OpenApiString>())
             {
-                var fullEnumMemberName = $"F:{fullTypeName}.{enumMemberName}";
+                var originalEnumMemberName = enumMember.Value;
+                var lowerEnumMemberName = originalEnumMemberName.ToLower(); // Приведение имени к нижнему регистру
+                lowerEnumValues.Add(new OpenApiString(lowerEnumMemberName)); // Добавляем преобразованное значение в новый список
+
+                var fullEnumMemberName = $"F:{fullTypeName}.{originalEnumMemberName}"; // Используем оригинальное имя для поиска комментария
 
                 var enumMemberComments = _xmlComments.Descendants("member")
                     .FirstOrDefault(m => m.Attribute("name")!.Value.Equals(fullEnumMemberName, StringComparison.OrdinalIgnoreCase));
@@ -44,10 +51,17 @@ public class EnumTypesSchemaFilter : ISchemaFilter
                 var summary = enumMemberComments.Descendants("summary").FirstOrDefault();
                 if (summary == null) continue;
 
-                schema.Description += $"<li><i>{enumMemberName}</i> - {summary.Value.Trim()}</li>";
+                schema.Description += $"<li><i>{lowerEnumMemberName}</i> - {summary.Value.Trim()}</li>"; // Используем имя в нижнем регистре в описании
             }
 
             schema.Description += "</ul>";
+
+            // Заменяем оригинальные значения enum на новые, приведённые к нижнему регистру
+            schema.Enum.Clear();
+            foreach (var lowerEnumValue in lowerEnumValues)
+            {
+                schema.Enum.Add(lowerEnumValue);
+            }
         }
     }
 }
